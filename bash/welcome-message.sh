fafab723ebbfda741839f037d00137813e59e79f
Rithin Yaser <200413419@student.georgianc.on.ca>
2020-06-14 16:30:59 -0400

    Welcome message

---
+++ bash/welcome-message.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
+# Task 3: Add the time and day of the week to the welcome message using the format shown below
+#   Use a format like this:
+#   It is weekday at HH:MM AM.
+# Task 4: Set the title using the day of the week
+#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
+#   You will need multiple tests to set a title
+#   Invent your own titles, do not use the ones from this example
+
+###############
+# Variables   #
+###############
+
+#Removed title from this line
+myname=$USER
+hostname=$(hostname)
+#Replaced myname and hostname with $USER and $(hostname)
+
+hours=$(date +%H)
+min=$(date +%M)
+meridian=$(date +%p)
+day=$(date +%A)
+#Created variables hours,min,meridian and day and assigned the values of time to it
+
+[ $day == "Monday" ] && title="Overlord"
+[ $day == "Tuesday" ] && title="Master"
+[ $day == "Wednesday" ] && title="Superman"
+[ $day == "Thursday" ] && title="Iron man"
+[ $day == "Friday" ] && title="Kick-ass"
+[ $day == "Saturday" ] && title="Batman"
+[ $day == "Sunday" ] && title="Nobody"
+
+#Assigned a title to each day of the week
+
+
+###############
+# Main        #
+###############
+cat <<EOF
+
+Welcome to planet $hostname, "$title $myname!"
+It is $day at $hours:$min $meridian.
+
+EOF
